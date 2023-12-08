9712439444e8173d49e45d12dbf18dde591b6228
a.zaferany <a.zaferany79@gmail.com>
2023-12-08 16:26:03 +0330

    ci: add initial ci actions

---
+++ src/RamzPardakht.Infrastructure/DbContexts/ProjectDbContext.cs
@@ -0,0 +1,88 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Internal;
+using RamzPardakht.ApplicationCore.Contracts;
+using RamzPardakht.ApplicationCore.Entities;
+
+namespace RamzPardakht.Infrastructure.DbContexts;
+
+public class ProjectDbContext : DbContext, IProjectDbContext
+{
+    public ProjectDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // https://stackoverflow.com/questions/63063207/ef-core-setqueryfilter-reverse-isactive-to-isdeleted-in-onmodelcreating
+        foreach (var entityType in modelBuilder.Model.FindEntityTypes(typeof(ISoftDeletable)))
+        {
+            Expression<Func<ISoftDeletable, bool>> filterExpression = entity => !entity.IsDeleted;
+
+            entityType.SetQueryFilter(Expression.Lambda(filterExpression.Body, filterExpression.Parameters));
+        }
+    }
+
+    public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = default)
+    {
+        ChangeTracker.DetectChanges();
+
+        SetEntitiesContracts(cancellationToken);
+
+        ChangeTracker.AutoDetectChangesEnabled = false;
+
+        var result = await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+
+        ChangeTracker.AutoDetectChangesEnabled = true;
+
+        return result;
+    }
+
+    private void SetEntitiesContracts(CancellationToken cancellationToken)
+    {
+        var httpContext = this.GetService<IHttpContextAccessor>().HttpContext;
+        var dateTimeProvider = this.GetService<ISystemClock>();
+
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (entry.Entity is ISoftDeletable)
+            {
+                if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.CurrentValues[nameof(ISoftDeletable.IsDeleted)] = true;
+                    entry.CurrentValues[nameof(ISoftDeletable.DeletedOn)] = dateTimeProvider.UtcNow;
+                }
+            }
+
+            if (entry.Entity is ITimeable)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    if (httpContext?.User?.Identity?.IsAuthenticated == true)
+                    {
+                        entry.CurrentValues[nameof(ITimeable.CreatedById)] = httpContext.User.Identity.Name;
+                    }
+
+                    entry.CurrentValues[nameof(ITimeable.CreatedOn)] = dateTimeProvider.UtcNow;
+                }
+                else if (httpContext != null && httpContext.User != null && entry.State == EntityState.Modified)
+                {
+                    if (httpContext?.User?.Identity?.IsAuthenticated == true)
+                    {
+                        entry.CurrentValues[nameof(ITimeable.ModifiedById)] = httpContext.User.Identity.Name;
+                    }
+
+                    entry.CurrentValues[nameof(ITimeable.ModifiedOn)] = dateTimeProvider.UtcNow;
+                }
+            }
+        }
+    }
+}
---
+++ src/RamzPardakht.WebApi/Common/HttpClientBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Options;
+using Prometheus;
+using Serilog.HttpClient;
+
+namespace RamzPardakht.WebApi.Common;
+
+public static class HttpClientBuilderExtensions
+{
+    /// <summary>
+    /// Configures the HttpMessageHandler pipeline to collect Prometheus metrics.
+    /// </summary>
+    public static IServiceCollection UseHttpClientLogRequestResponse(
+        this IServiceCollection services,
+        Action<RequestLoggingOptions> configureOptions = null)
+    {
+        if (configureOptions == null)
+            configureOptions = (Action<RequestLoggingOptions>) (options => { });
+        services.Configure(null,configureOptions);
+
+        services.TryAddTransient<LoggingDelegatingHandler>(s =>
+            new LoggingDelegatingHandler(s.GetRequiredService<IOptionsSnapshot<RequestLoggingOptions>>().Get(null),
+                forHttpClientFactory: true));
+
+        return services.ConfigureAll<HttpClientFactoryOptions>(optionsToConfigure =>
+            optionsToConfigure.HttpMessageHandlerBuilderActions.Add(builder =>
+                builder.AdditionalHandlers.Add(
+                    builder.Services.GetRequiredService<LoggingDelegatingHandler>())));
+
+    }
+}

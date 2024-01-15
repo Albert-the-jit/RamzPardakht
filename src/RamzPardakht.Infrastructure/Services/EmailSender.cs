2bcc990e5d27d232295c27bbc8eed8a79cfb9925
Ali Zaferany <a.zaferany79@gmail.com>
2024-01-15 22:15:21 +0330

    fix : fix EmailSender.cs

--- src/RamzPardakht.Infrastructure/Services/EmailSender.cs
+++ src/RamzPardakht.Infrastructure/Services/EmailSender.cs
@@ -18,7 +18,7 @@ public class EmailSender :IEmailSender
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
         var message = new EmailMessage();
-        message.From = "zaferanii.ir";
+        message.From = "onboarding@resend.dev";
         message.To.Add( email);
         message.Subject = subject;
         message.HtmlBody = htmlMessage;
